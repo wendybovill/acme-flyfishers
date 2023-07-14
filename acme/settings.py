4e7d5bc2b698b3d4a6dcb1c587e73e0a490ed1dd
wendybovill <wendybovill@gmail.com>
2023-07-14 15:03:17 +0000

    changed html files for shop and base and index renamed images changed urls shop and css

--- acme/settings.py
+++ acme/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
