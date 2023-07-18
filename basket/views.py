6247eee00904f104dcc7301e89ebcb51f50f730f
wendybovill <wendybovill@gmail.com>
2023-07-18 14:11:01 +0000

    adding views.py for basket

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
+from django.contrib import messages
+from products.models import Product
 
-# Create your views here.
+
+def view_basket(request):
+    """ View to view basket """
+
+    return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ add product qty to basket """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+        messages.success(request,
+                         (f'Updated {product.name} '
+                          f'quantity to {basket[item_id]}'))
+    else:
+        basket[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your basket')
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
