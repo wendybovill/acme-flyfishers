5215adb93be90a6a91c60f71cf07492aefd9cc19
wendybovill <wendybovill@gmail.com>
2023-07-19 09:35:09 +0000

    added models.py to profiles

--- profiles/models.py
+++ profiles/models.py
@@ -4,4 +4,49 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django_countries.fields import CountryField
 
+class UserProfile(models.Model):
+    """
+    User Profile model for maintaining order history
+    and other information including delivery
+    """
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    default_full_name = models.CharField(max_length=200, null=True, blank=True)
+
+    default_email = models.EmailField(max_length=254, null=True, blank=True)
+
+    default_street_address1 = models.CharField(max_length=80,
+                                               null=True, blank=True)
+
+    default_street_address2 = models.CharField(max_length=80,
+                                               null=True, blank=True)
+
+    default_town_or_city = models.CharField(max_length=40,
+                                            null=True, blank=True)
+
+    default_county = models.CharField(max_length=80,
+                                      null=True, blank=True)
+
+    default_postcode = models.CharField(max_length=20,
+                                        null=True, blank=True)
+
+    default_country = CountryField(blank_label='Country',
+                                   null=True, blank=True)
+
+    default_phone_number = models.CharField(max_length=20,
+                                            null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    User profile creation or updating
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # If user already exists then the profile is updated (saved)
+    instance.userprofile.save()
