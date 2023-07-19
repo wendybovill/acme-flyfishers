a04acfe17a42f3e914191068ea82df037c4cde90
wendybovill <wendybovill@gmail.com>
2023-07-19 09:17:37 +0000

    added profiles django files

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from django_countries.fields import CountryField
+
+from shop.models import Product
