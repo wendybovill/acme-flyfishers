08d1468162344a9de32b500e876c7a1f23897ae5
wendybovill <wendybovill@gmail.com>
2023-07-20 23:55:07 +0000

    setting changed

--- acme/wsgi.py
+++ acme/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acme.settings')
 
 application = get_wsgi_application()
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+
