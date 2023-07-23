0ab43bf633c16af7963f5820c885cbf1b9a70b31
wendybovill <wendybovill@gmail.com>
2023-07-23 11:10:18 +0000

    troubleshooting emails redirecting to 500 error

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,7 +1,5 @@
-from django.core.validators import EmailValidator
 from django import forms
-from .widgets import CustomClearableFileInput
-from .models import UserProfile, ProfileContactForm
+from .models import UserProfile
 
 
 class UserProfileForm(forms.ModelForm):
@@ -45,60 +43,6 @@ class UserProfileForm(forms.ModelForm):
             'aria-label'] = 'Country'
         self.fields['default_phone_number'].widget.attrs[
             'aria-label'] = 'Phone Number'
-
-        for field in self.fields:
-            if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = ('border-black '
-                                                        'rounded-0 '
-                                                        'profile-form-input')
-        self.fields[field].label = False
-
-
-class ProfileContactForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        exclude = ('user',)
-
-    def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
-        super().__init__(*args, **kwargs)
-        placeholders = {
-            'default_full_name': 'Full Name',
-            'default_email': 'Email Address',
-            'user_email_subject': 'Subject',
-            'user_email_message': 'Message',
-            'user_contact_phone_number': 'Phone Number',
-            'image': 'Upload Image',
-            'image_url': 'Link to Image',
-        }
-
-        self.fields['default_full_name'].widget.attrs['autofocus'] = True
-        self.fields['default_full_name'].widget.attrs[
-            'aria-label'] = 'Full Name'
-        self.fields['default_email'].widget.attrs[
-            'aria-label'] = 'Email Address'
-        self.fields['user_email_subject'].widget.attrs[
-            'aria-label'] = 'Subject'
-        self.fields['user_email_message'].widget.attrs[
-            'aria-label'] = 'Message'
-        self.fields['user_contact_phone_number'].widget.attrs[
-            'aria-label'] = 'Phone Number'
-        self.fields['image'].widget.attrs[
-            'aria-label'] = 'Upload Image'
-        self.fields['image_url'].widget.attrs[
-            'aria-label'] = 'Link to Image'
-
-        forms.ImageField(label='Image',
-                         required=False,
-                         widget=CustomClearableFileInput)
 
         for field in self.fields:
             if field != 'default_country':
--- profiles/models.py
+++ profiles/models.py
@@ -1,11 +1,8 @@
-import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django_countries.fields import CountryField
-from django.utils.html import html, mark_safe
-from django.conf import settings
 
 
 class UserProfile(models.Model):
@@ -56,45 +53,3 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     # If user already exists then the profile is updated (saved)
     instance.userprofile.save()
-
-
-class ProfileContactForm(models.Model):
-    """
-    A form for the registered user to contact the shop owners
-    """
-    user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
-                             null=True, blank=True,
-                             related_name='full_name')
-
-    response = models.BooleanField(blank=True, null=True, default=False)
-
-    default_full_name = models.CharField(max_length=100,
-                                         null=True, blank=True)
-
-    default_email = models.EmailField(max_length=254,
-                                      null=True, blank=True)
-
-    user_email_subject = models.CharField(max_length=100,
-                                          null=False, blank=False)
-
-    user_email_message = models.TextField(max_length=2000,
-                                                    null=False,blank=False)
-
-    user_contact_phone_number = models.CharField(max_length=20,
-                                                blank=True,null=True)
-
-    image = models.ImageField(upload_to='email_images/',
-                              null=True, blank=True)
-
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.default_email_address
-
-    def image_preview(self):
-        return mark_safe(f'<img src = "{self.image.url}" width = "150"/>')
-
-    def get_friendly_name(self):
-        return self.default_email
