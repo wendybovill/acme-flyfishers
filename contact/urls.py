b1f0f83306e05e5d957b0c6d763ffd0079871672
wendybovill <wendybovill@gmail.com>
2023-07-26 23:16:47 +0000

    httpres pg test

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.contact, name='contact'),
     path('email_sent/', views.email_sent,
-         name='email_sent'),
+         name='email_sent/'),
 ]
