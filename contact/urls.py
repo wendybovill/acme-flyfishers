b113be76bb0a3857e4da42ea71587b59035b852c
wendybovill <wendybovill@gmail.com>
2023-07-26 17:14:35 +0000

    updates to emails:

--- contact/urls.py
+++ contact/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('contact',
          views.contact, name='contact'),
-    path('email_sent', views.email_sent,
+    path('email_sent/', views.email_sent,
          name='email_sent'),
 ]
