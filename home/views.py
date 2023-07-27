40df22d2861a5a0f1311147287c07585feb8b0e9
wendybovill <wendybovill@gmail.com>
2023-07-27 02:13:09 +0000

    fixing toasts

--- home/views.py
+++ home/views.py
@@ -25,6 +25,13 @@ def index(request):
     return render(request, 'home/index.html', context)
 
 
+def handler403(request):
+    response = render_to_response('403.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 403
+    return response
+
+
 def handler404(request):
     response = render_to_response('404.html', {},
                                   context_instance=RequestContext(request))
