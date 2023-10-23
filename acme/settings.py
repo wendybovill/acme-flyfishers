b51268d8106af92a303da5520be1780e4c2290c6
wendybovill <wendybovill@gmail.com>
2023-10-23 23:21:50 +0000

    making small change to settings allowed hosts

--- acme/settings.py
+++ acme/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['acmeflydressers-882ac53eca0c.herokuapp.com',
+ALLOWED_HOSTS = ['acmeflydressers-882ac53eca0c.herokuapp.com', 'www.demoshop.wendywebweaver.com','demoshop.wendywebweaver.com', 
                  '8000-wendybovill-milestonepr-l6cezvkc4vc.ws-eu101.gitpod.io',
                  'localhost',
                  '127.0.0.1',
